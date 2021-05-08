09ac906bfa3610f740a2b09c215ea4fa24c52ca3
gx-oy <83329076+gx-oy@users.noreply.github.com>
2021-05-08 13:55:45 +0800

    Update 35.cpp

--- 35.cpp
+++ 35.cpp
@@ -24,9 +24,9 @@ class Person
       //this->p_age=100;//当在函数后面加了const，这一行会报错，此时这是一个常函数
       this->p_b=100;//此时不会报错，因为在p_b的声明前加了 mutable
     }
-  void hanshi()
+  void hanshu()
   {
-    
+    //常函数
   }
     int p_age;
     mutable int p_b;
@@ -39,7 +39,8 @@ void t1()
 }
 void t2()
 {
-  const Person p;//常对象
+  const Person p;//常对象，如果实在vscode里面这个写法是不行的，因为定义的常对象是需要初始化的，可以换一种方法创建对象
+  //const Person p=Person();//这种方式是可以通过编译的
   p.p_a=100;//会报错
   p.p_b=100;//不会报错
   p.hanshu();//此时报错，因为常对象只能调用常函数，因为在非 常函数中是默认允许修改对象属性值的，而常对象却不允许，如果常对象调用非 常函数，有点自相矛盾的意思了
